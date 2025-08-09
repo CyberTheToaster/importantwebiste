d2b72276b869ed690fcb7129940ac9f30988d538
CyberTheToaster <jwozcode123@gmail.com>
2025-08-09 17:03:15 -0400

    hehe v2

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let Body = document.getElementById("e")
-window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=OVy_xXorcwYYPR_F"
+let funnibutton = document.getElementById("free robux")
+funnibutton.onclick = function() {
+    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=OVy_xXorcwYYPR_F"
+}
+let coolman = document.getElementById("coolman")
+coolman.onclick = function() {
+    window.alert("connection established: requesting insider or mod")
+}
