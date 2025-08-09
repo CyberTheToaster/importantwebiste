6fc399417e8fd69ba3206c47e826b35c4213a417
CyberTheToaster <jwozcode123@gmail.com>
2025-08-09 15:54:34 -0400

    will dis work??

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
 let Body = document.getElementById("e")
-Body.onload = function() {
-    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=OVy_xXorcwYYPR_F"
-}
+window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=OVy_xXorcwYYPR_F"
